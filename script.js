a5b787d8e54eb21b8656ffe6f93f01749e49ee88
thms200 <thms0504@gmail.com>
2019-08-25 13:17:02 +0900

    modified class name etc

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 
-let first = document.getElementsByClassName('first')
-let section = document.getElementsByClassName('section')
+let title = document.getElementsByClassName('title')
+let title_Name = document.getElementsByClassName('title_Name')
 let content = document.getElementsByClassName('content')
 let arrow = document.getElementsByClassName('arrow-left')
 
 
-for(let i = 0; i < first.length; i++) {
+for(let i = 0; i < title.length; i++) {
     
-    first[i].addEventListener('click', function() {
-        if(first[i].getAttribute("class") === 'first') {
-            first[i].style.backgroundColor = "rgb(0, 62, 255)"
-            section[i].style.color= "white"
+    title[i].addEventListener('click', function() {
+        if(title[i].getAttribute("class") === 'title') {
+            title[i].style.backgroundColor = "rgb(0, 62, 255)"
+            title_Name[i].style.color= "white"
             content[i].style.transition = "1s"
             content[i].style.height = "200px"
             arrow[i].style.borderLeft = "5px solid transparent"
             arrow[i].style.borderRight = "5px solid transparent"
             arrow[i].style.borderTop = "5px solid white"
-            first[i].setAttribute("class", "first click");
+            title[i].setAttribute("class", "title click");
         } else {
-            first[i].style.backgroundColor = "rgb(246, 246, 246)"
-            section[i].style.color= "black"
+            title[i].style.backgroundColor = "rgb(246, 246, 246)"
+            title_Name[i].style.color= "black"
             content[i].style.transition = "1s"
             content[i].style.height = "0px"
             arrow[i].style.borderTop = "5px solid transparent"
             arrow[i].style.borderBottom = "5px solid transparent"
             arrow[i].style.borderLeft = "5px solid black"
-            first[i].setAttribute("class", "first");
+            title[i].setAttribute("class", "title");
         }
     })
 }
